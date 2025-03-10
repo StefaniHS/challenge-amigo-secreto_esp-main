63c8bd41d9db11d1a01a299c46382e84302904f4
StefaniHS <sstefanihs@gmail.com>
2025-03-10 00:20:37 -0500

    Primer commit

---
+++ app.js
@@ -0,0 +1,5 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//funcion Agregar nombres
+//funcion Validar entrada
+//Visualizar la lista
+//funcion Sorteo aleatorio
