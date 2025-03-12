833c1ea1308fa696fc6025d5d324dfb42dc6a4e3
StefaniHS <sstefanihs@gmail.com>
2025-03-11 23:36:39 -0500

    funcion validarEntrada

--- app.js
+++ app.js
@@ -1,19 +1,33 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-//funcion Agregar nombres
+
 //funcion Validar entrada
+//funcion Agregar nombres
 //Visualizar la lista
 //funcion Sorteo aleatorio
 
-
-
-function agregarAmigo() {
-    alert("click desde la funcion");
-}
+//let nombreDeAmigo= prompt("Escribe un nombre");
 
 function asignarTextoElemento(elemento, texto) {
     let elementoHtml = document.querySelector(elemento);
     elementoHtml.innerHTML = texto;
+    return;
 }
 
 asignarTextoElemento('h1', 'Amigo Secreto');
 asignarTextoElemento('h2', 'Digite el nombre de sus amigos');
+
+//dentro de esta funcion debe estar pedir, validar si es string, almacenar y mostrar el nombre
+function validarEntrada() {
+    let nombreDeAmigo = String(document.getElementById("amigo").value);
+
+    //nos permite ingresar nombres con masyusculas, minusculas, tildes y espacios
+    let valoresPermitidos = /^[a-zA-ZáéíóúÁÉÍÓÚ\s]+$/;
+
+    if (!valoresPermitidos.test(nombreDeAmigo)) {
+        asignarTextoElemento("p", "El valor ingresado no es válido. Por favor, ingresa un nombre válido.");
+    } else {
+        asignarTextoElemento("p", nombreDeAmigo)
+        return;
+    }
+}
+
