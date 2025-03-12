5c8ebdae74ed4ac5d0d6f56282983f7f0e86435f
StefaniHS <sstefanihs@gmail.com>
2025-03-11 21:33:02 -0500

    Funcion asignar texto

--- app.js
+++ app.js
@@ -3,3 +3,17 @@
 //funcion Validar entrada
 //Visualizar la lista
 //funcion Sorteo aleatorio
+
+
+
+function agregarAmigo() {
+    alert("click desde la funcion");
+}
+
+function asignarTextoElemento(elemento, texto) {
+    let elementoHtml = document.querySelector(elemento);
+    elementoHtml.innerHTML = texto;
+}
+
+asignarTextoElemento('h1', 'Amigo Secreto');
+asignarTextoElemento('h2', 'Digite el nombre de sus amigos');
