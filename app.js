9ce4a185b5ebd9ff8cd21f50e9e26b57674b3b73
StefaniHS <sstefanihs@gmail.com>
2025-03-14 00:42:45 -0500

    Funcion mostrar lista de amigos

--- app.js
+++ app.js
@@ -4,30 +4,52 @@
 //funcion Agregar nombres
 //Visualizar la lista
 //funcion Sorteo aleatorio
+//boton reiniciar sorteo
+//eliminar a los sorteados para que no se repitan
+//opcion eliminar ingreso y que cada participante se muestre con un numero
+//y que no permita ingresar dos nombres iguales
 
-//let nombreDeAmigo= prompt("Escribe un nombre");
+//aqui se almacenan los nombres ingresados
+let amigo = [];
 
-function asignarTextoElemento(elemento, texto) {
-    let elementoHtml = document.querySelector(elemento);
-    elementoHtml.innerHTML = texto;
-    return;
-}
 
-asignarTextoElemento('h1', 'Amigo Secreto');
-asignarTextoElemento('h2', 'Digite el nombre de sus amigos');
+function agregarAmigo() {
+    let nombreDeAmigo = document.getElementById("amigo").value;
 
-//dentro de esta funcion debe estar pedir, validar si es string, almacenar y mostrar el nombre
-function validarEntrada() {
-    let nombreDeAmigo = String(document.getElementById("amigo").value);
+    //validación de datos donde nos permite ingresar nombres con mayusculas, minusculas y tildes
+    let valoresPermitidos = /^[a-zA-ZáéíóúÁÉÍÓÚ]+$/;
 
-    //nos permite ingresar nombres con masyusculas, minusculas, tildes y espacios
-    let valoresPermitidos = /^[a-zA-ZáéíóúÁÉÍÓÚ\s]+$/;
-
+    //Se esta evaluando si el valor de la variable nombreDeAmigo no cumple con una condición establecida 
+    // por la expresión valoresPermitidos.
     if (!valoresPermitidos.test(nombreDeAmigo)) {
-        asignarTextoElemento("p", "El valor ingresado no es válido. Por favor, ingresa un nombre válido.");
+        alert("El valor ingresado no es válido. Por favor, ingresa un nombre.");
+        limpiarCaja();
+
     } else {
-        asignarTextoElemento("p", nombreDeAmigo)
+        amigo.push(nombreDeAmigo);
+        limpiarCaja();
+        mostrarListaAmigos();
         return;
     }
 }
+
+function limpiarCaja() {
+    document.getElementById("amigo").value = "";
+    //coloca el cursor al inicio de la caja para ingresar el siguiente nombre
+    document.getElementById("amigo").focus();
+}
+
+function mostrarListaAmigos() {
+    let listaAmigos = document.getElementById("listaAmigos");
+    listaAmigos.innerHTML = "";
+
+    for (let i = 0; i < amigo.length; i++) {
+        let item = document.createElement("li");
+        item.textContent = amigo[i];
+        listaAmigos.appendChild(item);
+    }
+}
+
+
+
 
